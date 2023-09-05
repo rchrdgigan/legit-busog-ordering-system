7d46bd244ea0198e61520ba4669e196cee67bc6d
Richard Gigantone <rchrdgigantone@gmail.com>
2023-09-05 23:45:45 +0800

    Refactor login and register and Added some updates

--- LBOderingSys/settings.py
+++ LBOderingSys/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/mediafiles/'
